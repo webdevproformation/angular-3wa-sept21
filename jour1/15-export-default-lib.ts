cca846705c87d1aadbff2455f2b2f57782c6b313
Malik H <malik.h@webdevpro.net>
2021-09-27 16:40:20 +0200

    export default

---
+++ jour1/15-export-default-lib.ts
@@ -0,0 +1,8 @@
+// 15-export-default-lib.ts
+
+
+export default class ComposantHome{
+    constructor(private titre : string , private liste : Array<string>){}
+}
+
+// si vous avez 1 seul élément à exporter 
---
+++ jour1/15-export-default.ts
@@ -0,0 +1,8 @@
+// 15-export-default.ts
+// 15-export-default-lib.ts
+
+import ComposantHome from "./15-export-default-lib";
+
+const index = new ComposantHome("accueil" , ["node", "angular"]);
+
+console.log(index);
