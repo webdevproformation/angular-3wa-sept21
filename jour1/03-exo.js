3c7583a840239e1867f718e4e3afad23b797d4c2
Malik H <malik.h@webdevpro.net>
2021-09-27 10:36:33 +0200

    correction

---
+++ jour1/03-exo.js
@@ -0,0 +1,2 @@
+const tableau = ["lundi", "mardi", "mercredi"];
+tableau.forEach(jour => console.log(`${jour[0].toUpperCase()}${jour.slice(1)}`));
---
+++ jour1/03-exo.ts
@@ -0,0 +1,23 @@
+const tableau = ["lundi", "mardi", "mercredi"] ;
+
+// parcourir chaque élément du tableau et l'afficher dans la console avec un majuscule en 1ère lettre
+
+// for()
+// while()
+
+tableau.forEach(  jour  => console.log( `${jour[0].toUpperCase()}${jour.slice( 1 )}` ))
+
+// fonction fléchée 
+// template string avec ${}
+// transformer ce fichier en .js
+// cd jour1
+// tsc 03-exo.ts --target es6 --removeComments
+// transformer mon fichier ts => js ET exécuté dans nodejs 
+// tsc 03-exo.ts --target es6 --removeComments && node 03-exo.js
+
+/**
+autre solution possible 
+for (let element of tableau){
+    console.log(element.charAt(0).toUpperCase()+element.slice( 1 ));
+}
+ */
