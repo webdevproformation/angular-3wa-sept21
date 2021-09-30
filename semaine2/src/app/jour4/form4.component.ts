5aba5a967135f7dfbc8ebd32723beb8d8309328a
Malik H <malik.h@webdevpro.net>
2021-09-30 17:32:04 +0200

    fin de journée !!

--- semaine2/src/app/jour4/form4.component.ts
+++ semaine2/src/app/jour4/form4.component.ts
@@ -6,18 +6,23 @@ import {addressValidator} from "./customValidator"
   selector: 'form4',
   template: `
     <h3>Exo</h3>
-    <form [formGroup]="form">
+    <form [formGroup]="form" (ngSubmit)="formSubmit()">
       <input type="text" name="password" class="form-control" formControlName="password">
       <p>{{password?.errors | json}}</p>
       <input type="text" name="text" class="form-control" formControlName="service">
       <p>{{service?.errors | json}}</p>
+      <input type="submit" class="btn btn-light border border-dark mb-4" [disabled]="form.invalid">
+      <p>{{form.valid | json}}</p>
     </form>
-    
-  `,
-  styles: [
-  ]
+  `
 })
 export class Form4Component implements OnInit {
+
+  public formSubmit(){
+    if(this.form.valid){
+      console.log(this.form.value); 
+    }
+  }
 
   public form : FormGroup = new FormGroup({
     password : new FormControl("", addressValidator.validPassword),
