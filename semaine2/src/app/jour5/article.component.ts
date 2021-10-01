c7075a1ec9424014a0ee5000647ac6f67054a683
Malik H <malik.h@webdevpro.net>
2021-10-01 17:35:51 +0200

    correction

--- semaine2/src/app/jour5/article.component.ts
+++ semaine2/src/app/jour5/article.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'jour5-article',
   template: `
     <div [innerHTML]="html"></div>
+    <ul>
+      <li *ngFor="let categorie of categories">{{ categorie }}</li>
+    </ul>
   `
 })
 export class ArticleComponent implements OnInit {
-  public html : string = ""
+  public html : string = "";
+  public categories : Array<string> = [];
   constructor( public http : HttpClient) { }
+
+  public getCategories() {
+    return this.http.get("http://localhost:3000/categories");
+  }
+
   ngOnInit(): void {
     this.http.get("http://localhost:3000/articles").subscribe( 
       (x : any) => {
@@ -22,6 +32,10 @@ export class ArticleComponent implements OnInit {
         }).join("") // [ , , ] => ""
        }
     )
+  this.getCategories().subscribe( (data : any ) => {
+    this.categories = data 
+  })
+
   }
 
 }
